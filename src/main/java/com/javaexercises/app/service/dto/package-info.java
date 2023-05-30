1bc7ddc44a3bab60eabfd54b8336598b62c218ed
Hugo Perez <hugo.perez@bearstudio.fr>
2023-05-30 09:54:34 +0200

    Initial version of javaExercises generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/javaexercises/app/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.javaexercises.app.service.dto;
---
+++ src/main/java/com/javaexercises/app/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.javaexercises.app.web.rest.vm;
